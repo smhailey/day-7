d91eaadf163301013d030e2b69a8895d730a6873
smhailey <smhailey@msn.com>
2019-06-11 09:27:25 -0600

    Created Chewy Granola Bar

---
+++ app.js
@@ -0,0 +1,12 @@
+let chewyGranolaBar = {
+  'ingredient1': 'granola',
+  'ingredient2': 'semisweetChocolateChips',
+  'ingredient3': 'cornSyrup',
+  'ingredient4': 'brownRiceCrisp',
+  'ingredient5': 'invertSugar',
+  'ingredient6': 'sugar',
+  'ingredient7': 'cornSyrupSolids',
+  'ingredient8': 'glycerin',
+  'ingredient9': 'soybeanOil',
+};
+console.log(chewyGranolaBar)
